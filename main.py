f523004c87dc81c4fe2e8f7a4bdc15127ae8968f
Josh Markus <joshuajmarkus@gmail.com>
2017-06-18 22:46:41 -0700

    Added rotate-by form and imported caesar function

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+
+form = """
+    <form method="post">
+
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+            
+                <label>Rotate by:
+                    <input type="text" name="rot" value=0 />
+                </label>
+                <textarea name="text"> </textarea>
+                <input type="submit" value="Submit" />  
+        </body>
+    </html>
+</form>
+"""
+
+
+@app.route("/")
+
+def index():
+    return form
+
+app.run()
